1311e320e9a721b196d20568608a1653fe294096
Brayan Vargas <brayanvargasbarajas@gmail.com>
2025-10-10 17:46:15 -0500

    fix(pagos): correct redirect URL scheme for iOS

--- src/app/pagos/pages/pago-redirect/pago-redirect.page.ts
+++ src/app/pagos/pages/pago-redirect/pago-redirect.page.ts
@@ -70,7 +70,7 @@ export class PagoRedirectPage implements OnInit {
     this.route.queryParams.subscribe(params => {
       this._codigo.set(params['codigo']);
       if (params['desde_ios'] && this._codigo()) {
-        window.location.href = `com.unab.reservas://pagos/reservas?codigo=${this._codigo()}`;
+        window.location.href = `com.unab.reservas://pagos-redirect/reservas?codigo=${this._codigo()}`;
         return;
       }
       if (this._codigo()) {
